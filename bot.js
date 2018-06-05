85167dc6cc06eaf91803bd0bd7cfc03ebc65de37
HairuoLiu <liu.3502@osu.edu>
2018-06-05 10:33:28 -0400

    10:30/5

--- bot.js
+++ bot.js
@@ -1,48 +1,66 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client();
+const config = require("./config.json");
+const fs = require("fs")
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+
+bot.on("ready", () => {
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
-
-
 
 bot.on("message", async message => {
 
-  message.content.toLowerCase;
-  
   if (message.author.bot) return;
-  msg = message.content.toLowerCase();
-  let prefix = '-';
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
   
-  if(msg.startWith(prefix + "leetcode" )){
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
     
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
   
+  // if(msg.startWith(prefix + "leetcode" )){
+    
+  // }
   
-  if(msg.startWith(prefix + "laicode" )){
-    message.channel.send("https://code.laioffer.com/ui/#/app/problem/");
-  }
-  
-  
-  
-  
-  if (message.channel.type === "dm") return;
-
-
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
-
+  // if(msg.startWith(prefix + "laicode" )){
+  //   message.channel.send("https://code.laioffer.com/ui/#/app/problem/");
+  // }
   
   
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
 });
 
 bot.login(process.env.token);
